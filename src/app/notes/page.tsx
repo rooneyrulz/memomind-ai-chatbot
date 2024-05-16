634c965e99638c554d7d26c4fc2043ecf9423d95
ruzny <roony0782045336@gmail.com>
2024-05-16 20:41:41 +0530

    setup clerk authentication

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MemoMind - Notes",
+};
+
+export default function Notes() {
+  return (
+    <div>Notes Page!!</div>
+  )
+}
