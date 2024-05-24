1d50a2350a05d8b7e1cc5393bbe8afb17f178f02
ruzny <roony0782045336@gmail.com>
2024-05-24 19:51:00 +0530

    added navbar & home page

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import NavBar from "./NavBar";
+
+// export const metadata: Metadata = {
+//   title: "MemoMind",
+//   description: "The AI Notes Application",
+// };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <NavBar />
+      <main className="p-4 max-w-7xl m-auto">{children}</main>
+    </>
+  );
+}
