f414d7fd3c8bbde9ff4afa7afdcfd97b22fc15ea
ruzny <roony0782045336@gmail.com>
2024-05-27 22:50:51 +0530

    setup add-note functionalities

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,11 +1,17 @@
+import prisma from "@/lib/db/prisma";
+import { auth } from "@clerk/nextjs/server";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "MemoMind - Notes",
 };
 
-export default function Notes() {
-  return (
-    <div>Notes Page!!</div>
-  )
+export default async function NotesPage() {
+  const { userId } = auth();
+
+  if (!userId) throw Error("UserId undefined!");
+
+  const notes = await prisma.note.findMany({ where: { userId } });
+
+  return <div>{JSON.stringify(notes)}</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-5">
       <div className="flex items-center gap-4">
-        <Image src={logo} alt="FlowBrain logo" width={100} height={100} />
+        <Image src={logo} alt="MemoMind logo" width={100} height={100} />
         <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           MemoMind
         </span>
       </div>
       <p className="max-w-prose text-center">
-        An AI powered memo app, built with OpenAI, Pipecone, Next.js, Shadcn UI,
+        An AI powered memo app, built with OpenAI, Pinecone, Next.js, Shadcn UI,
         Clerk, and more.
       </p>
       <Button size="lg" asChild>
