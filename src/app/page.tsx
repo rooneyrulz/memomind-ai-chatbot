b1428c75fb8ee6818112a1573f9e9e96d4df6c78
ruzny.rulzz <ruzny.rulzz@gmail.com>
2024-09-18 13:23:53 +0530

    added gradient-bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   const { userId } = auth();
 
   if (userId) redirect("/notes");
-
+  
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-5">
+    <main className="flex h-screen flex-col items-center justify-center gap-5 bg-gradient-to-r from-yellow-400 via-gray-50 to-teal-300">
       <div className="flex items-center gap-4">
         <Logo width={100} height={100} />
         <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
